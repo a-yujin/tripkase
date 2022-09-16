7b8775507afd9238445cb443b50908d4df4f542f
Miso <Miso@DESKTOP-74S97PJ>
2022-09-16 17:56:02 +0900

    도메인, 컨트롤러 작성
    room domain 작성, trip domain 작성, tripReply domain작성
    tripController.java
    - 게시글 작성 페이지
    resources, css 폴더생성
    - trip.css 생성

--- src/main/java/com/tripKase/kh/room/domain/Room.java
+++ src/main/java/com/tripKase/kh/room/domain/Room.java
@@ -1,5 +1,215 @@
 package com.tripKase.kh.room.domain;
 
-public class Room {
+import java.sql.Date;
 
+public class Room {
+	private int roomNo; // 숙소 순번
+	private String roomName; // 숙소 이름
+	private String roomArea; // 숙소 지역(전체,서울,경기도,인천 등)
+	private String roomType; // 숙소 종류 (전체,모텔,호텔,펜션,리조트)
+	private int roomPeople; // 숙소 인원 (입실가능 인원)
+	private String roomPet; // 반려견 동반여부
+	private Date checkIn; // 숙소 체크인 날짜
+	private Date checkOut; // 숙소 체크아웃 날짜
+	private int roomPrice; // 숙소 가격
+	private String roomAddress; // 숙소 주소
+	private String roomTel; // 숙소 전화번호
+	private String roomMap; // 숙소 지도
+	private String roomCar; // 숙소 주차 정보
+	private String roomLocation; // 숙소 오시는길
+	private String roomGuide; // 객실 정보
+	private String roomFileName; // 숙소 변경전 사진 이름
+	private String roomFileRename; // 숙소 변경후 사진 이름
+	private String roomFilePath; // 숙소 사진 경로
+	private String roomStatus; // 숙소 게시여부
+	
+	public Room() {}
+
+	public Room(int roomNo, String roomName, String roomArea, String roomType, int roomPeople, String roomPet,
+			Date checkIn, Date checkOut, int roomPrice, String roomAddress, String roomTel, String roomMap,
+			String roomCar, String roomLocation, String roomGuide, String roomFileName, String roomFileRename,
+			String roomFilePath, String roomStatus) {
+		super();
+		this.roomNo = roomNo;
+		this.roomName = roomName;
+		this.roomArea = roomArea;
+		this.roomType = roomType;
+		this.roomPeople = roomPeople;
+		this.roomPet = roomPet;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+		this.roomPrice = roomPrice;
+		this.roomAddress = roomAddress;
+		this.roomTel = roomTel;
+		this.roomMap = roomMap;
+		this.roomCar = roomCar;
+		this.roomLocation = roomLocation;
+		this.roomGuide = roomGuide;
+		this.roomFileName = roomFileName;
+		this.roomFileRename = roomFileRename;
+		this.roomFilePath = roomFilePath;
+		this.roomStatus = roomStatus;
+	}
+
+	public int getRoomNo() {
+		return roomNo;
+	}
+
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+
+	public String getRoomName() {
+		return roomName;
+	}
+
+	public void setRoomName(String roomName) {
+		this.roomName = roomName;
+	}
+
+	public String getRoomArea() {
+		return roomArea;
+	}
+
+	public void setRoomArea(String roomArea) {
+		this.roomArea = roomArea;
+	}
+
+	public String getRoomType() {
+		return roomType;
+	}
+
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+
+	public int getRoomPeople() {
+		return roomPeople;
+	}
+
+	public void setRoomPeople(int roomPeople) {
+		this.roomPeople = roomPeople;
+	}
+
+	public String getRoomPet() {
+		return roomPet;
+	}
+
+	public void setRoomPet(String roomPet) {
+		this.roomPet = roomPet;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public void setCheckIn(Date checkIn) {
+		this.checkIn = checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+
+	public void setCheckOut(Date checkOut) {
+		this.checkOut = checkOut;
+	}
+
+	public int getRoomPrice() {
+		return roomPrice;
+	}
+
+	public void setRoomPrice(int roomPrice) {
+		this.roomPrice = roomPrice;
+	}
+
+	public String getRoomAddress() {
+		return roomAddress;
+	}
+
+	public void setRoomAddress(String roomAddress) {
+		this.roomAddress = roomAddress;
+	}
+
+	public String getRoomTel() {
+		return roomTel;
+	}
+
+	public void setRoomTel(String roomTel) {
+		this.roomTel = roomTel;
+	}
+
+	public String getRoomMap() {
+		return roomMap;
+	}
+
+	public void setRoomMap(String roomMap) {
+		this.roomMap = roomMap;
+	}
+
+	public String getRoomCar() {
+		return roomCar;
+	}
+
+	public void setRoomCar(String roomCar) {
+		this.roomCar = roomCar;
+	}
+
+	public String getRoomLocation() {
+		return roomLocation;
+	}
+
+	public void setRoomLocation(String roomLocation) {
+		this.roomLocation = roomLocation;
+	}
+
+	public String getRoomGuide() {
+		return roomGuide;
+	}
+
+	public void setRoomGuide(String roomGuide) {
+		this.roomGuide = roomGuide;
+	}
+
+	public String getRoomFileName() {
+		return roomFileName;
+	}
+
+	public void setRoomFileName(String roomFileName) {
+		this.roomFileName = roomFileName;
+	}
+
+	public String getRoomFileRename() {
+		return roomFileRename;
+	}
+
+	public void setRoomFileRename(String roomFileRename) {
+		this.roomFileRename = roomFileRename;
+	}
+
+	public String getRoomFilePath() {
+		return roomFilePath;
+	}
+
+	public void setRoomFilePath(String roomFilePath) {
+		this.roomFilePath = roomFilePath;
+	}
+
+	public String getRoomStatus() {
+		return roomStatus;
+	}
+
+	public void setRoomStatus(String roomStatus) {
+		this.roomStatus = roomStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Room [roomNo=" + roomNo + ", roomName=" + roomName + ", roomArea=" + roomArea + ", roomType=" + roomType
+				+ ", roomPeople=" + roomPeople + ", roomPet=" + roomPet + ", checkIn=" + checkIn + ", checkOut="
+				+ checkOut + ", roomPrice=" + roomPrice + ", roomAddress=" + roomAddress + ", roomTel=" + roomTel
+				+ ", roomMap=" + roomMap + ", roomCar=" + roomCar + ", roomLocation=" + roomLocation + ", roomGuide="
+				+ roomGuide + ", roomFileName=" + roomFileName + ", roomFileRename=" + roomFileRename
+				+ ", roomFilePath=" + roomFilePath + ", roomStatus=" + roomStatus + "]";
+	}
 }
---
+++ src/main/java/com/tripKase/kh/trip/domain/TripReply.java
@@ -0,0 +1,100 @@
+package com.tripKase.kh.trip.domain;
+
+import java.sql.Date;
+
+public class TripReply {
+	private int tReplyNo; // 여행소통 댓글 순번
+	private int repTripNo; // 여행소통 게시글 순번
+	private String tReplyContents; // 여행소통 댓글 내용
+	private String tReplyWriter; // 여행소통 댓글 작성자
+	private Date tReplyCreate; // 여행소통 댓글 작성일
+	private Date tReplyUpdate; // 여행소통 댓글 수정일
+	private int tReplyCount; // 여행소통 댓글수
+	private String tReplyStatus; // 여행소통 댓글 존재여부
+	
+	public TripReply() {}
+
+	public TripReply(int tReplyNo, int repTripNo, String tReplyContents, String tReplyWriter, Date tReplyCreate,
+			Date tReplyUpdate, int tReplyCount, String tReplyStatus) {
+		super();
+		this.tReplyNo = tReplyNo;
+		this.repTripNo = repTripNo;
+		this.tReplyContents = tReplyContents;
+		this.tReplyWriter = tReplyWriter;
+		this.tReplyCreate = tReplyCreate;
+		this.tReplyUpdate = tReplyUpdate;
+		this.tReplyCount = tReplyCount;
+		this.tReplyStatus = tReplyStatus;
+	}
+
+	public int gettReplyNo() {
+		return tReplyNo;
+	}
+
+	public void settReplyNo(int tReplyNo) {
+		this.tReplyNo = tReplyNo;
+	}
+
+	public int getRepTripNo() {
+		return repTripNo;
+	}
+
+	public void setRepTripNo(int repTripNo) {
+		this.repTripNo = repTripNo;
+	}
+
+	public String gettReplyContents() {
+		return tReplyContents;
+	}
+
+	public void settReplyContents(String tReplyContents) {
+		this.tReplyContents = tReplyContents;
+	}
+
+	public String gettReplyWriter() {
+		return tReplyWriter;
+	}
+
+	public void settReplyWriter(String tReplyWriter) {
+		this.tReplyWriter = tReplyWriter;
+	}
+
+	public Date gettReplyCreate() {
+		return tReplyCreate;
+	}
+
+	public void settReplyCreate(Date tReplyCreate) {
+		this.tReplyCreate = tReplyCreate;
+	}
+
+	public Date gettReplyUpdate() {
+		return tReplyUpdate;
+	}
+
+	public void settReplyUpdate(Date tReplyUpdate) {
+		this.tReplyUpdate = tReplyUpdate;
+	}
+
+	public int gettReplyCount() {
+		return tReplyCount;
+	}
+
+	public void settReplyCount(int tReplyCount) {
+		this.tReplyCount = tReplyCount;
+	}
+
+	public String gettReplyStatus() {
+		return tReplyStatus;
+	}
+
+	public void settReplyStatus(String tReplyStatus) {
+		this.tReplyStatus = tReplyStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "TripReply [tReplyNo=" + tReplyNo + ", repTripNo=" + repTripNo + ", tReplyContents=" + tReplyContents
+				+ ", tReplyWriter=" + tReplyWriter + ", tReplyCreate=" + tReplyCreate + ", tReplyUpdate=" + tReplyUpdate
+				+ ", tReplyCount=" + tReplyCount + ", tReplyStatus=" + tReplyStatus + "]";
+	}
+}
