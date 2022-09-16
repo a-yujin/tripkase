02e0d775540352a6bfdb5326eb9c17b365fdcd16
yangy <yangy@DESKTOP-6RHR9J6>
2022-09-16 17:54:32 +0900

    양유진 220916 커밋
    Attraction, Notice, NoticeReply, QnA 도메인 작성

--- src/main/java/com/tripKase/kh/notice/domain/Notice.java
+++ src/main/java/com/tripKase/kh/notice/domain/Notice.java
@@ -1,5 +1,92 @@
 package com.tripKase.kh.notice.domain;
 
-public class Notice {
+import java.sql.Date;
 
+public class Notice {
+	private int noticeNo; // 공지 번호
+	private String noticeWriter; // 공지 작성자
+	private String noticeType; // 공지 유형
+	private String noticeTitle; // 공지 제목
+	private Date nCreateDate; // 공지 등록일
+	private Date nUpdateDate; // 공지 수정일
+	private String nContents; // 공지 내용
+	private String nFileName; // 공지 파일 이름
+	private String nFileRename; // 공지 파일 변경된 이름
+	private String nFilePath; // 공지 파일 경로
+	private int noticeCount; // 공지 게시글 수
+	
+	public int getNoticeNo() {
+		return noticeNo;
+	}
+	public void setNoticeNo(int noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+	public String getNoticeWriter() {
+		return noticeWriter;
+	}
+	public void setNoticeWriter(String noticeWriter) {
+		this.noticeWriter = noticeWriter;
+	}
+	public String getNoticeType() {
+		return noticeType;
+	}
+	public void setNoticeType(String noticeType) {
+		this.noticeType = noticeType;
+	}
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+	public Date getnCreateDate() {
+		return nCreateDate;
+	}
+	public void setnCreateDate(Date nCreateDate) {
+		this.nCreateDate = nCreateDate;
+	}
+	public Date getnUpdateDate() {
+		return nUpdateDate;
+	}
+	public void setnUpdateDate(Date nUpdateDate) {
+		this.nUpdateDate = nUpdateDate;
+	}
+	public String getnContents() {
+		return nContents;
+	}
+	public void setnContents(String nContents) {
+		this.nContents = nContents;
+	}
+	public String getnFileName() {
+		return nFileName;
+	}
+	public void setnFileName(String nFileName) {
+		this.nFileName = nFileName;
+	}
+	public String getnFileRename() {
+		return nFileRename;
+	}
+	public void setnFileRename(String nFileRename) {
+		this.nFileRename = nFileRename;
+	}
+	public String getnFilePath() {
+		return nFilePath;
+	}
+	public void setnFilePath(String nFilePath) {
+		this.nFilePath = nFilePath;
+	}
+	public int getNoticeCount() {
+		return noticeCount;
+	}
+	public void setNoticeCount(int noticeCount) {
+		this.noticeCount = noticeCount;
+	}
+	
+	@Override
+	public String toString() {
+		return "Notice [noticeNo=" + noticeNo + ", noticeWriter=" + noticeWriter + ", noticeType=" + noticeType
+				+ ", noticeTitle=" + noticeTitle + ", nCreateDate=" + nCreateDate + ", nUpdateDate=" + nUpdateDate
+				+ ", nContents=" + nContents + ", nFileName=" + nFileName + ", nFileRename=" + nFileRename
+				+ ", nFilePath=" + nFilePath + ", noticeCount=" + noticeCount + "]";
+	}
 }
--- src/main/java/com/tripKase/kh/qna/domain/QnA.java
+++ src/main/java/com/tripKase/kh/qna/domain/QnA.java
@@ -1,5 +1,107 @@
 package com.tripKase.kh.qna.domain;
 
-public class QnA {
+import java.sql.Date;
 
+public class QnA {
+	private int questionNo; // 질문 번호
+	private String questionWriter; // 질문 작성자
+	private String questionType; // 질문 유형
+	private String questionTitle; // 질문 제목
+	private Date qCreateDate; // 질문 등록일
+	private String answerYN; // 답변 여부
+	private String qContents; // 질문 내용
+	private String qFileName; // 질문 파일 이름
+	private String qFileRename; // 질문 파일 변경된 이름
+	private String qFilePath; // 질문 파일 경로
+	private String answerWriter; // 답변 작성자
+	private Date aCreateDate; // 답변 등록일
+	private String aContents; // 답변 내용
+	
+	public int getQuestionNo() {
+		return questionNo;
+	}
+	public void setQuestionNo(int questionNo) {
+		this.questionNo = questionNo;
+	}
+	public String getQuestionWriter() {
+		return questionWriter;
+	}
+	public void setQuestionWriter(String questionWriter) {
+		this.questionWriter = questionWriter;
+	}
+	public String getQuestionType() {
+		return questionType;
+	}
+	public void setQuestionType(String questionType) {
+		this.questionType = questionType;
+	}
+	public String getQuestionTitle() {
+		return questionTitle;
+	}
+	public void setQuestionTitle(String questionTitle) {
+		this.questionTitle = questionTitle;
+	}
+	public Date getqCreateDate() {
+		return qCreateDate;
+	}
+	public void setqCreateDate(Date qCreateDate) {
+		this.qCreateDate = qCreateDate;
+	}
+	public String getAnswerYN() {
+		return answerYN;
+	}
+	public void setAnswerYN(String answerYN) {
+		this.answerYN = answerYN;
+	}
+	public String getqContents() {
+		return qContents;
+	}
+	public void setqContents(String qContents) {
+		this.qContents = qContents;
+	}
+	public String getqFileName() {
+		return qFileName;
+	}
+	public void setqFileName(String qFileName) {
+		this.qFileName = qFileName;
+	}
+	public String getqFileRename() {
+		return qFileRename;
+	}
+	public void setqFileRename(String qFileRename) {
+		this.qFileRename = qFileRename;
+	}
+	public String getqFilePath() {
+		return qFilePath;
+	}
+	public void setqFilePath(String qFilePath) {
+		this.qFilePath = qFilePath;
+	}
+	public String getAnswerWriter() {
+		return answerWriter;
+	}
+	public void setAnswerWriter(String answerWriter) {
+		this.answerWriter = answerWriter;
+	}
+	public Date getaCreateDate() {
+		return aCreateDate;
+	}
+	public void setaCreateDate(Date aCreateDate) {
+		this.aCreateDate = aCreateDate;
+	}
+	public String getaContents() {
+		return aContents;
+	}
+	public void setaContents(String aContents) {
+		this.aContents = aContents;
+	}
+	
+	@Override
+	public String toString() {
+		return "QnA [questionNo=" + questionNo + ", questionWriter=" + questionWriter + ", questionType=" + questionType
+				+ ", questionTitle=" + questionTitle + ", qCreateDate=" + qCreateDate + ", answerYN=" + answerYN
+				+ ", qContents=" + qContents + ", qFileName=" + qFileName + ", qFileRename=" + qFileRename
+				+ ", qFilePath=" + qFilePath + ", answerWriter=" + answerWriter + ", aCreateDate=" + aCreateDate
+				+ ", aContents=" + aContents + "]";
+	}
 }
