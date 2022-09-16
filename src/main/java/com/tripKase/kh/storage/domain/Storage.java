1dd51ea0604b71db59e41806e8a6a0abae46ae21
YANGWOO <YANGWOO@LAPTOP>
2022-09-16 17:50:25 +0900

    양우석 첫번째 커밋
    레스토랑 스케줄 스토리지 도메인 작성

--- src/main/java/com/tripKase/kh/storage/domain/Storage.java
+++ src/main/java/com/tripKase/kh/storage/domain/Storage.java
@@ -1,5 +1,10 @@
 package com.tripKase.kh.storage.domain;
 
 public class Storage {
-
+	private int stoNo;
+	private String stoKategorie;
+	private String stoContentsId;
+	private String stoContentsName;
+	private String stoContentsImage;
+	private String memberId;
 }
