18ff2abf67499e3ccb947980bd471f80cf708c5a
hjkl1 <hjkl1@DESKTOP-QMNSEUR>
2022-09-25 21:37:16 +0900

    양우석 220925 21:36
    보관함 삭제 기능 추가
    
    (기능은 구현했지만 아직 미완성)

--- src/main/java/com/tripKase/kh/storage/controller/StorageController.java
+++ src/main/java/com/tripKase/kh/storage/controller/StorageController.java
@@ -22,18 +22,6 @@ public class StorageController {
 	@Autowired
 	private StorageService stoService;
 	
-//	/**
-//	 * 보관함 데이터 불러오기
-//	 * @param session 세션 정보
-//	 * @return 보관함데이터 리스트
-//	 */
-//	@RequestMapping(value = "/storage/getStorageData.tripkase")
-//	public List<Storage> getStorageData(HttpSession session) {
-//		Member member = (Member)session.getAttribute("loginMember");
-//		String memberId = member.getMemberId();
-//
-//		return  stoService.getStorageData(memberId);
-//	}
 
 	/**
 	 *  보관함 등록
@@ -47,7 +35,6 @@ public class StorageController {
 			String memberId = member.getMemberId();
 			paramMap.put("memberId", memberId);
 			int result = stoService.insertStorage(paramMap);
-//			System.out.println(paramMap);
 			if(result > 0) {
 				mv.setViewName("storage/storageMainPage");
 			}
@@ -86,13 +73,13 @@ public class StorageController {
 	@RequestMapping(value = "/storage/storageView.tripkase", method=RequestMethod.GET)
 	public ModelAndView showStorageView(ModelAndView mv, HttpSession session) {
 		try {
-			Member member = (Member)session.getAttribute("loginMember");
+			Member member = (Member)session.getAttribute("loginMember");				// 세션에서 멤버아이디 가져옴
 			String memberId = member.getMemberId();
-			List<Storage> resDataList = stoService.getResDataByMemberId(memberId);
+			List<Storage> resDataList = stoService.getResDataByMemberId(memberId);		// 멤버 아이디별로 각 컨텐츠 정보 가져옴
 			List<Storage> roomDataList = stoService.getRoomDataByMemberId(memberId);
 			List<Storage> attrDataList = stoService.getAttrDataByMemberId(memberId);
 			//System.out.println(resDataList);
-			mv.addObject("resDataList",resDataList);
+			mv.addObject("resDataList",resDataList);		// 가져온 데이터를 jsp에 뿌리기
 			mv.addObject("roomDataList",roomDataList);
 			mv.addObject("attrDataList",attrDataList);
 			mv.setViewName("storage/storageMainPage");
@@ -103,19 +90,6 @@ public class StorageController {
 		return mv;
 	}
 	
-	
-	
-//	내가 만든 겟스토리지데이터 마크원버전
-//	@RequestMapping(value = "/storage/storageView.tripkase", method=RequestMethod.GET)
-//	public ModelAndView getStorageData(ModelAndView mv, HttpSession session) {
-//		Member member = (Member)session.getAttribute("loginMember");
-//		String memberId = member.getMemberId();
-//		List<Storage> stoList = stoService.getStorageData(memberId);
-//		mv.addObject("stoList",stoList);
-//		System.out.println(stoList);
-//		mv.setViewName("storage/storageMainPage");
-//		return mv;
-//	}
 
 
 }
